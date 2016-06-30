e24621a8ccbeb3a2867aac80fae9945022d84d18
Caius Brindescu <brindesc@oregonstate.edu>
2016-06-29 20:03:15 -0700

    Enriched the class

--- src/main/scala/com/brindescu/jdtfacade/RichClass.scala
+++ src/main/scala/com/brindescu/jdtfacade/RichClass.scala
@@ -3,6 +3,12 @@ package com.brindescu.jdtfacade
 import org.eclipse.jdt.core.dom.TypeDeclaration
 
 class RichClass(private val node: TypeDeclaration) {
+
+	def getFullyQualifiedName(): String =
+		node.resolveBinding.getQualifiedName
+
+	def getDescriptor(): String =
+		"L" + getFullyQualifiedName().replace('.','/')
 }
 
 object RichClass {
