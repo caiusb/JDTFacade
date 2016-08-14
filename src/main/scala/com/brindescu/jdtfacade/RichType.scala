a52fde25460d772159ff3014f3e410cbcbfc6890
Caius Brindescu <brindesc@oregonstate.edu>
2016-08-14 14:23:37 -0700

    Will not fail if bindings are not present

--- src/main/scala/com/brindescu/jdtfacade/RichType.scala
+++ src/main/scala/com/brindescu/jdtfacade/RichType.scala
@@ -15,8 +15,13 @@ class RichType(private val t: Type) {
 			case _ => ""
 		}
 
-	private def getObjectCode(t: SimpleType): String =
-		"L" + t.getName.resolveBinding.asInstanceOf[ITypeBinding].getErasure.getQualifiedName.replace('.','/') + ";"
+	private def getObjectCode(t: SimpleType): String = {
+		val binding = t.getName.resolveBinding
+		if (binding != null)
+			return "L" + binding.asInstanceOf[ITypeBinding].getErasure.getQualifiedName.replace('.', '/') + ";"
+		else
+			return "L" + t.getName.getFullyQualifiedName + ";"
+	}
 
 	private def getObjectCode(t: QualifiedType): String =
 		"L" + t.getName.getFullyQualifiedName.replace('.','/') + ";"
