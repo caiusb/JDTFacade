fd104f5c84e2b9173366c2fcd876943a43f38df5
Caius Brindescu <brindesc@oregonstate.edu>
2017-01-26 16:16:05 -0800

    Added method to get a node at a line number

--- src/main/scala/com/brindescu/jdtfacade/RichCompilationUnit.scala
+++ src/main/scala/com/brindescu/jdtfacade/RichCompilationUnit.scala
@@ -1,6 +1,6 @@
 package com.brindescu.jdtfacade
 
-import org.eclipse.jdt.core.dom.{CompilationUnit, TypeDeclaration}
+import org.eclipse.jdt.core.dom.{ASTNode, CompilationUnit, NodeFinder, TypeDeclaration}
 
 import scala.collection.JavaConversions._
 
@@ -9,6 +9,12 @@ class RichCompilationUnit(private val cu: CompilationUnit) {
 	def getDeclaredTypes: List[TypeDeclaration] =
 		cu.getStructuralProperty(CompilationUnit.TYPES_PROPERTY).asInstanceOf[java.util.List[_]].iterator
 			.map { m => m.asInstanceOf[TypeDeclaration] } toList
+
+	def findNodeAtLine(l: Int): ASTNode = {
+		val start = cu.getPosition(l, 0)
+		val end = cu.getPosition(l + 1, 0) - 1
+		NodeFinder.perform(cu, start, end - start)
+	}
 }
 
 object RichCompilationUnit {
